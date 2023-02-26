652f76b3006696b576410ae8f0cbb9effe4bcbef
Trairath <6210110712@psu.ac.th>
2023-02-27 00:20:42 +0700

    create webpage

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
-const   http = require('http')
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
+const mainpage = fs.readFileSync(`${__dirname}/webpages/index.html`)
+const signinpage = fs.readFileSync(`${__dirname}/webpages/sign_in.html`)
 
 const   sever = http.createServer((req,res)=>{
-    res.write("testting sever")
-    res.end()
+    const path = req.url
+    console.log("url = ",path)
+    
+    if(path === "/" || path === "/home" ){
+        res.end(mainpage)
+    }
+    else if(path === "/sign_in"){
+        res.end(signinpage)
+    }
+    else{
+        res.writeHead(404)
+        res.end("Not Found")
+    }
+    
 })
 sever.listen(5000,()=>{
     console.log("sever running")
