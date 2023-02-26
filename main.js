2d80562bc5f69e57d6a8254c71a9e07f8e3d52fa
Trairath <6210110712@psu.ac.th>
2023-02-26 23:11:16 +0700

    add nodemon

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const   http = require('http')
 
-const   sever = http.createServer(function(req,res){
+const   sever = http.createServer((req,res)=>{
     res.write("testting sever")
     res.end()
 })
-sever.listen(5000)
+sever.listen(5000,()=>{
+    console.log("sever running")
+})
