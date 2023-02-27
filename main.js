7fe25bc11704d447f78ef363c60182e85037e029
Trairath <6210110712@psu.ac.th>
2023-02-27 08:56:26 +0700

    sql post test

--- main.js
+++ main.js
@@ -1,6 +1,44 @@
-const http = require('http')
-const path = require('path')
-const fs = require('fs')
+const express = require('express')
+const cors = require('cors')
+const app =express()
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+app.use(cors())
+
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'tri',
+    database: 'hsm_db'
+});
+
+app.post('/register',jsonParser,function(req,res,next){
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        connection.execute(
+            'INSERT INTO user(email,password,firstname,surname) VALUE (?,?,?,?)',
+            [req.body.email,hash,req.body.firstname,req.body.surname],
+            function(err, results, fields) {
+              if (err){
+                res.json({status:'error',massage: err})
+                return
+              }
+              res.json({status:'pass'})
+            }
+          );
+    });  
+})
+
+app.post('/register',jsonParser,function(req,res,next){
+    
+})
+
+
+app.listen(5000, () => console.log("Server is Running..."));
+
+/*
 const mainpage = fs.readFileSync(`${__dirname}/webpages/index.html`)
 const signinpage = fs.readFileSync(`${__dirname}/webpages/sign_in.html`)
 const Register = fs.readFileSync(`${__dirname}/webpages/Register.html`)
@@ -27,3 +65,4 @@ const   sever = http.createServer((req,res)=>{
 sever.listen(5000,()=>{
     console.log("sever running")
 })
+*/
